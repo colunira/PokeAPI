2166b9002aeeebf33a3fc824a0ca2c06f1ec8a16
colunira <jul.iskierka@gmail.com>
2020-01-27 19:25:24 +0100

    Nie wiem

--- app/src/main/java/com/example/pokeapi/model/Pokemon.kt
+++ app/src/main/java/com/example/pokeapi/model/Pokemon.kt
@@ -1,5 +1,10 @@
 package com.example.pokeapi.model
 
+import com.google.gson.annotations.SerializedName
+
 class Pokemon {
-    val name: String = "name"
+    @SerializedName("id") val id: Int = 0
+    @SerializedName("name") val name: String = ""
+    @SerializedName("base_experience") val experience: Int = 0
+    @SerializedName("height") val height: Int = 0
 }
